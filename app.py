8d23d897951370e6613a5990c4a23fee18e838da
suaf <suaf2020@163.com>
2023-02-19 15:38:52 +0800

    fast init

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：fast_project 
+@File    ：app.py
+@Author  ：Su af
+@Date    ：2023/2/19 15:24 
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ application/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：fast_project 
+@File    ：__init__.py.py
+@Author  ：Su af
+@Date    ：2023/2/19 15:25 
+"""
+
+if __name__ == '__main__':
+    pass
